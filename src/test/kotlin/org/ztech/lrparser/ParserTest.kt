e21b5c96e1ee2ff5c594df460aef2ccb84a6f682
S Merzlyakov <merzliakov@gmail.com>
2021-07-28 16:37:47 +0400

    тесты

--- src/test/kotlin/org/ztech/lrparser/ParserTest.kt
+++ src/test/kotlin/org/ztech/lrparser/ParserTest.kt
@@ -1,6 +1,92 @@
 package org.ztech.lrparser
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
 class ParserTest {
+
+    /**
+     * Тест на корректность проверки грамматик
+     */
+    @Test
+    fun checkGrammar() {
+
+        // Список терминальных символов пустой
+        assertThrows<Exception>({ "Should throw an Exception" }) {
+            Grammar(
+                terminals = setOf(),
+                nonTerminals = setOf(),
+                productions = mapOf(),
+                startProduction = 'S'
+            )
+        }
+
+        // Список терминальных символов должен содержать только латинские символы в нижнем регистре.
+        assertThrows<Exception>({ "Should throw an Exception" }) {
+            Grammar(
+                terminals = setOf('A'),
+                nonTerminals = setOf(),
+                productions = mapOf(),
+                startProduction = 'S'
+            )
+        }
+
+        // Список нетерминальных символов пустой.
+        assertThrows<Exception>({ "Should throw an Exception" }) {
+            Grammar(
+                terminals = setOf('a'),
+                nonTerminals = setOf(),
+                productions = mapOf(),
+                startProduction = 'S'
+            )
+        }
+
+        // Список нетерминальных символов должен содержать только латинские символы в верхнем регистре.
+        assertThrows<Exception>({ "Should throw an Exception" }) {
+            Grammar(
+                terminals = setOf('a'),
+                nonTerminals = setOf('b'),
+                productions = mapOf(),
+                startProduction = 'S'
+            )
+        }
+
+        // Проверим что для каждого нетерминального символа имеется продукция
+        assertThrows<Exception>({ "Should throw an Exception" }) {
+            Grammar(
+                terminals = setOf('a'),
+                nonTerminals = setOf('A', 'B', 'S'),
+                productions = mapOf('A' to setOf("a")),
+                startProduction = 'S'
+            )
+        }
+
+        // Левая часть продукции должна содержать только нетерминальные символы.
+        assertThrows<Exception>({ "Should throw an Exception" }) {
+            Grammar(
+                terminals = setOf('a', 'b', 's'),
+                nonTerminals = setOf('A', 'B', 'S'),
+                productions = mapOf(
+                    'A' to setOf("a"), 'B' to setOf("b"),
+                    'S' to setOf("s"), 'a' to setOf("A")
+                ),
+                startProduction = 'S'
+            )
+        }
+
+        // Правая часть продукций должна состоять только из символов грамматики.
+        assertThrows<Exception>({ "Should throw an Exception" }) {
+            Grammar(
+                terminals = setOf('a', 'b', 's'),
+                nonTerminals = setOf('A', 'B', 'S'),
+                productions = mapOf(
+                    'A' to setOf(""), 'B' to setOf("b"),
+                    'S' to setOf("s", "e")
+                ),
+                startProduction = 'S'
+            )
+        }
+    }
 
     /*@Test
     fun doFirstTest() {
